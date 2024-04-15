89c071a4c2b78a18b588217e487d7d7221aa3b5b
Tim Krause <timkrausedev@gmail.com>
2024-04-15 11:46:45 -0400

    initial commit

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = (): JSX.Element => {
+
+  return (
+    <footer>This is the footer</footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './styles.module.scss';
+
+const Header = (): JSX.Element => {
+
+  return (
+    <header className={styles.header}>This is the header</header>
+  );
+}
+
+export default Header;
