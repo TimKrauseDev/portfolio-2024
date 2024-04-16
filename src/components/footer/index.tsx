6ec98af4b6e5f0fbdda170ffa244bb9fab2cae91
Tim Krause <timkrausedev@gmail.com>
2024-04-16 19:31:06 -0400

    add socials to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import styles from './styles.module.scss';
 import Container from "@/components/ui/container";
+import Linkedin from "@/components/ui/svgs/linkedin";
+import Github from "@/components/ui/svgs/github";
+import { Cell, Grid } from "@faceless-ui/css-grid";
 
 const Footer = (): JSX.Element => {
 
   return (
     <footer className={styles.footer}>
       <Container>
-        <p>© 2024 Tim Krause</p>
+        <Grid>
+          <Cell cols={6}>
+            <p>© {new Date().getFullYear()} Tim Krause</p>
+          </Cell>
+          <Cell cols={6} className={styles.socialWrapper}>
+            <a
+              href='https://www.linkedin.com/in/timothyckrause/'
+              target="_blank"
+              rel="noopenner noreferrer"
+            >
+              <Linkedin />
+            </a>
+            <a
+              href='https://www.linkedin.com/in/timothyckrause/'
+              target="_blank"
+              rel="noopenner noreferrer"
+            >
+              <Github />
+            </a>
+          </Cell>
+        </Grid>
       </Container>
     </footer>
   );
