272bc0b12a68449cff55bb488cb8fe22ec78fe89
Tim Krause <timkrausedev@gmail.com>
2024-04-16 19:01:37 -0400

    update header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,6 +6,7 @@ import Logo from "@/components/ui/svgs/logo";
 import { Sling as Hamburger } from 'hamburger-react';
 import clsx from "clsx";
 import Button from "@/components/ui/button";
+import Link from "next/link";
 
 clsx(styles.nav, true && 'active');
 
@@ -16,14 +17,16 @@ const Header = (): JSX.Element => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Logo />
+        <Link href="/">
+          <Logo />
+        </Link>
       </div>
       <div className={styles.navWrapper}>
         <nav className={clsx(styles.nav, isOpen && styles.navActive)}>
           <ul className={styles.navItems}>
-            <li className={styles.navItem}><a href="#">About</a></li>
-            <li className={styles.navItem}><a href="#">Projects</a></li>
-            <Button to="/">Contact</Button>
+            <li className={styles.navItem}><a href="#about">About</a></li>
+            <li className={styles.navItem}><a href="#portfolio">Portfolio</a></li>
+            <Button to="mailto:timkrausedev@gmail.com" size="small">Contact</Button>
           </ul>
         </nav>
         <div className={styles.mobileBtn}>
