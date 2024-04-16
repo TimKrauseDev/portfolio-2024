6717ea5480f25494449afe3dda9f37c10b08a99c
Tim Krause <timkrausedev@gmail.com>
2024-04-16 19:02:28 -0400

    new svgs

---
+++ src/components/ui/svgs/underline.tsx
@@ -0,0 +1,28 @@
+const Underline = (): JSX.Element => {
+  return (
+    <svg width="79px" height="10px" viewBox="0 0 79 10" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <linearGradient x1="0%" y1="49.6977935%" x2="99.6977935%" y2="49.6977935%" id="linearGradient-1">
+          <stop stopColor="#3E5C76" stopOpacity="0" offset="0%"></stop>
+          <stop stopColor="#3E5C76" stopOpacity="0.10" offset="20%"></stop>
+          <stop stopColor="#3E5C76" stopOpacity="0.50" offset="50%"></stop>
+          <stop stopColor="#3E5C76" stopOpacity="0.84" offset="80%"></stop>
+          <stop stopColor="#3E5C76" offset="100%"></stop>
+        </linearGradient>
+      </defs>
+      <g stroke="none" strokeWidth="1" fill="" fillRule="evenodd">
+        <g transform="translate(-360.000000, -1388.000000)" fill="url(#linearGradient-1)">
+          <g transform="translate(0.000000, 1230.000000)">
+            <g  transform="translate(359.000000, 100.000000)">
+              <g  transform="translate(1.000000, 0.000000)">
+                <polygon id="Rectangle" points="0 58 69 58 79 68 0 68"></polygon>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+  </svg>
+  );
+}
+
+export default Underline;
