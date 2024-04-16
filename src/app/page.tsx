369ec7977e1a55e4ab07478f097e4f7c8d694212
Tim Krause <timkrausedev@gmail.com>
2024-04-16 19:05:45 -0400

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,20 @@
-'use client';
-
-import Spacer from '@/components/ui/spacer';
-import Button from '@/components/ui/button';
-import Container from "@/components/ui/container";
 import React from "react";
+import Spacer from '@/components/ui/spacer';
 import Hero from '@/components/hero';
-import Technology from '@/components/technology';
+import About from '@/components/about';
+import Portfolio from '@/components/portfolio';
+import Contact from "@/components/contact";
 
 const Home = (): JSX.Element => {
   return (
     <main>
-      <Spacer height={30} />
       <Hero />
-      <Spacer height={30} />
-      <Technology />
-      <Spacer height={30} />
-      <Container>
-        <p>Paragraph</p>
-        <p className='lead'>Lead Paragraph</p>
-        <a href="#">Link</a>
-        <h1>Heading One</h1>
-        <h2>Heading Two</h2>
-        <h3>Heading Three</h3>
-        <h4>Heading Four</h4>
-        <h5>Heading Five</h5>
-        <h6>Heading Six</h6>
-        <Spacer height={30} />
-        <Button variant='primary'>Primary</Button>
-        <Button variant='outline'>Outline</Button>
-        <Button variant='outline-dark'>Outline Dark</Button>
-        <Button variant='primary' to="#">Primary</Button>
-        <Button variant='outline' to="#">Outline</Button>
-        <Button variant='outline-dark' to="#">Outline Dark</Button>
-      </Container>
-      <Spacer height={30} />
+      <Spacer height={120} />
+      <About />
+      <Spacer height={120} />
+      <Portfolio />
+      <Spacer height={120} />
+      <Contact />
     </main>
   );
 }
