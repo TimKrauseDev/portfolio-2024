4b6bab99ca50efc4f9e67c3ccbd299091668ec57
Tim Krause <timkrausedev@gmail.com>
2024-04-16 19:03:47 -0400

    create 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import Button from '@/components/ui/button'
+import Container from '@/components/ui/container'
+import Spacer from '@/components/ui/spacer'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <section>
+      <Container>
+        <h1>404 - Page not found</h1>
+        <p className="lead-lg">Ops, this page does not exist.</p>
+        <Spacer height={30} />
+        <Button to="/">Home</Button>
+        <Spacer height={30} />
+
+      </Container>
+    </section>
+  )
+}
