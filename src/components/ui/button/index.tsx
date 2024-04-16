a2e0d1646eb127f662cb3c52140aa62941b553a2
Tim Krause <timkrausedev@gmail.com>
2024-04-16 19:00:39 -0400

    update buttons to be more dynamic

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -8,23 +8,44 @@ import styles from './styles.module.scss';
 type PropTypes = {
   children: string | number;
   variant?: 'primary' | 'outline' | 'dark' |'outline-dark';
+  size?: 'default' | 'small';
+  newTab?: boolean;
   to?: string;
   cb?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 
 }
 
 
-const Button = ({children, variant = 'primary', to, cb}: PropTypes): JSX.Element => {
+const Button = ({children, variant = 'primary', size = 'default', newTab = false, to, cb}: PropTypes): JSX.Element => {
 
-  console.log(variant);
-  if (to) {
+  // external link
+  if (to && newTab) {
     return (
-      <Link className={clsx(styles.button, styles[variant])} href={to}>{children}</Link>
+      <a
+        className={clsx(styles.button, styles[variant], styles[size])}
+        href={to}
+        target="_blank"
+        rel="noopenner noreferrer"
+      >{children}</a>
     );
   }
 
+  // internal link
+  if (to) {
+    return (
+      <Link
+        className={clsx(styles.button, styles[variant], styles[size])}
+        href={to}
+      >{children}</Link>
+    );
+  }
+
+
   return (
-    <button className={clsx(styles.button, styles[variant])} onClick={cb}>{children}</button>
+    <button
+      className={clsx(styles.button, styles[variant])}
+      onClick={cb}
+    >{children}</button>
   );
 }
 
