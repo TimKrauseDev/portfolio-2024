2dd2be45ad19763298127d3d98c1cbdd1638b4fe
Tim Krause <timkrausedev@gmail.com>
2024-04-18 13:57:52 -0400

    update lorem to copy

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -17,13 +17,13 @@ const About = (): JSX.Element => {
             <Underline />
             <Spacer height={30} />
             <h3>Frontend Developer</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro pariatur molestias sed laboriosam, saepe dolores earum sunt? Odio eaque blanditiis commodi quaerat eveniet eligendi corporis. Nam molestiae aliquid iste nobis!</p>
+            <p>As a frontend developer, I bring three years of dedicated experience to the table. I specialize in crafting engaging and responsive user interfaces that seamlessly blend design with functionality. From conceptualization to implementation, I thrive on turning ideas into intuitive digital experiences.</p>
             <Spacer height={30} />
             <h3>CMS</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro pariatur molestias sed laboriosam, saepe dolores earum sunt? Odio eaque blanditiis commodi quaerat eveniet eligendi corporis. Nam molestiae aliquid iste nobis!</p>
+            <p>Proficient in both Squarespace and Wordpress, I excel in leveraging content management systems to empower clients with dynamic and easily manageable websites. Whether it's customizing templates, optimizing performance, or ensuring seamless updates, I am adept at harnessing the full potential of CMS platforms to deliver tailored solutions.</p>
             <Spacer height={30} />
             <h3>Skills</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro pariatur molestias sed laboriosam, saepe dolores earum sunt? Odio eaque blanditiis commodi quaerat eveniet eligendi corporis. Nam molestiae aliquid iste nobis!</p>
+            <p>My skill set encompasses a diverse range of tools and technologies, with expertise in TypeScript and React at the forefront. Combining these with my proficiency in HTML, CSS, and JavaScript, I craft robust and interactive web applications that meet the evolving needs of modern users. With a keen eye for detail and a passion for innovation, I am committed to delivering high-quality solutions that exceed expectations.</p>
           </Cell>
           <Cell cols={6}>
             <img src={aboutImg.src} alt="Milkyway at night in Joshua Tree National Park" />
--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -17,7 +17,15 @@ const Portfolio = (): JSX.Element => {
           <Underline />
           <Spacer height={30} />
           <h3>Squarespace</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla beatae qui omnis veritatis error non, reprehenderit officia magnam dignissimos cum placeat, et amet, iure laudantium aspernatur. Quidem ratione enim dolorum.</p>
+          <p>
+            With a track record of supporting design teams on over 150 Squarespace websites, I've honed my skills in providing technical and programming assistance. From crafting custom components to implementing unique features, I collaborate closely to bring design visions to life.
+          </p>
+          <p>
+            My role extends beyond design, as I offer comprehensive support in email, web, and domain hosting. This includes everything from connecting and transferring custom domains to troubleshooting connectivity issues, ensuring clients' online presence remains seamless and reliable.
+          </p>
+          <p>
+            Additionally, my proficiency in search engine optimization (SEO) drives tangible results. By implementing best practices and offering tailored recommendations, I've successfully boosted the rankings of multiple clients on Google, enhancing their visibility and online performance.
+          </p>
           <Spacer height={30} />
           <Grid>
             <Cell cols={4} colsS={6}>
@@ -68,7 +76,12 @@ const Portfolio = (): JSX.Element => {
           </Grid>
           <Spacer height={60} />
           <h3>WordPress / Static</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla beatae qui omnis veritatis error non, reprehenderit officia magnam dignissimos cum placeat, et amet, iure laudantium aspernatur. Quidem ratione enim dolorum.</p>
+          <p>
+            I specialize in converting design files from Figma and Photoshop into custom WordPress websites and static sites, providing comprehensive support throughout the process. This includes assisting clients with content entry, technical support, and the creation of new features. Collaborating closely with designers, I ensure websites meet high standards of functionality, user experience, and compliance, including mobile responsiveness and WCAG 2.1 standards.
+          </p>
+          <p>
+            Moreover, I provide ongoing hosting support and implement effective SEO strategies to enhance clients' online visibility. This includes optimizing site performance, configuring plugins, and offering tailored recommendations to improve search engine rankings. Additionally, I've developed reusable custom React-based WordPress Gutenberg blocks, streamlining website buildouts for increased efficiency and customization to meet clients' needs.
+          </p>
           <Spacer height={30} />
           <Grid>
             <Cell cols={4} colsS={6}>
@@ -118,7 +131,7 @@ const Portfolio = (): JSX.Element => {
             </Cell>
           </Grid>
           <Spacer height={60} />
-          <h3>React</h3>
+          {/* <h3>React</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla beatae qui omnis veritatis error non, reprehenderit officia magnam dignissimos cum placeat, et amet, iure laudantium aspernatur. Quidem ratione enim dolorum.</p>
           <Spacer height={30} />
           <Grid>
@@ -145,7 +158,7 @@ const Portfolio = (): JSX.Element => {
               />
             </Cell>
           </Grid>
-          <Spacer height={60} />
+          <Spacer height={60} /> */}
       </Container>
     </section>
   );
